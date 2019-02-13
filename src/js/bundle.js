75d8cd71fae9259c2c1a3e9de0f3b665345139f6
TuanNguyen <xuan.tuan@1pac.vn>
2019-02-13 10:48:15 +0700

    fix some issue

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,17 +1,13 @@
-import { getUser, getAllRepos, renameRepo, deleteRepo, createRepo } from './promiseAPI';
+import { getUser, getAllRepos, renameRepo, deleteRepo, createRepo } from './promise-api';
 
-const accessKey = 'ee4fecbcfa2218525f059ed37b42a0e44cadaf88'
-const labelAccessKey = 'git-access-key'
 const inputNewRepo = document.querySelector('#input-new-repo')
-const imgUserAvatar = document.querySelector('#userAvatar')
-const userName = document.querySelector('#userName')
-const userEmail = document.querySelector('#userEmail')
-const reposList = document.querySelector('#repoList')
+const imgUserAvatar = document.querySelector('#user-avatar')
+const userName = document.querySelector('#user-name')
+const userEmail = document.querySelector('#user-email')
+const reposList = document.querySelector('#repo-list')
 
 //DOM manipulate
-const createElement = () => {}
-
-const deleteElement = (id) => {
+const deleteElement = id => {
   document.querySelector(`#repo${id}`).style.display = 'none'
 }
 
@@ -22,37 +18,38 @@ const showUserInf = ({ name, login, email, avatar_url }) => {
   userEmail.innerHTML = email
 }
 
-const addRepoItem = (repo) => {
+const addRepoItem = repo => {
   let template = document.querySelector('#repo-template')
-    let clone = template.content.cloneNode(true)
-    let repoItem = clone.querySelector('li')
-    let inputRepo = repoItem.querySelector('#repoName')
-    let removeRepo = repoItem.querySelector('#btnRemoveRepo')
+  let clone = template.content.cloneNode(true)
+  let repoItem = clone.querySelector('li')
+  let inputRepo = repoItem.querySelector('#repo-name')
+  let removeRepo = repoItem.querySelector('#btn-remove-repo')
 
-    repoItem.id = 'repo' + repo.id
-    inputRepo.value = repo.name
-    inputRepo.addEventListener('dblclick', function () { this.readOnly = false })
-    inputRepo.addEventListener('keypress', function (event) {
-      if (event.which === 13 || event.keyCode === 13) {
-        this.readOnly = true
-        renameRepo(repo.owner.login, repo.name, this.value)
-          .then(alert('U have edited success, it may take a while to completely rename'))
-          .catch(alert('something happen. Abort change'))
-      }
-    })
-    removeRepo.addEventListener('click', function () {
-      let confirmDeleteBox = confirm(`you sure you want to delete repo ${repo.name} ?????`)
-      if (confirmDeleteBox === true) {
-        deleteRepo(repo.owner.login, repo.name)
-          .then(deleteElement(repo.id))
-          .catch(alert('delete fail'))
-      }
-    })
-    reposList.appendChild(clone)
+  repoItem.id = `repo${repo.id}`
+  inputRepo.value = repo.name
+  inputRepo.addEventListener('dblclick', function () { this.readOnly = false })
+  inputRepo.addEventListener('keypress', function (event) {
+    if (event.which === 13 || event.keyCode === 13) {
+      this.readOnly = true
+      renameRepo(repo.owner.login, repo.name, this.value)
+        .catch(err => {
+          this.value = repo.name
+          alert(err)
+        })
+    }
+  })
+  removeRepo.addEventListener('click', function () {
+    let confirmDeleteBox = confirm(`you sure you want to delete repo ${repo.name} ?????`)
+    if (confirmDeleteBox === true) {
+      deleteRepo(repo)
+        .then(deleteElement(repo.id))
+    }
+  })
+  reposList.appendChild(clone)
 }
 
-const showRepos = (repos) => {
-  repos.map((repo) => {
+const showRepos = repos => {
+  repos.map(repo => {
     addRepoItem(repo)
   })
 }
@@ -62,9 +59,10 @@ const onKeyPressAccessKey = () => {
   inputNewRepo.addEventListener('keypress', function (event) {
     if (event.which === 13 || event.keyCode === 13) {
       let newRepoName = inputNewRepo.value
-      createRepo({name: newRepoName})
-        .then(data => {addRepoItem(data)})
-        inputNewRepo.value = ''
+      createRepo({ name: newRepoName })
+        .then(data => { addRepoItem(data) })
+        .catch(er => alert(er))
+      inputNewRepo.value = ''
     }
   })
 }
--- src/js/promise-api.js
+++ src/js/promise-api.js
@@ -1,19 +1,26 @@
 const hostURL = 'https://api.github.com'
+const accessKey = 'e007f511e4b8303e78435518e1a820b47a0d886a'
 
 //Promise
 export const callAPI = (endpoint, method, body) => {
   return fetch(`${hostURL}${endpoint}`, {
     method: method,
     headers: {
-      'Authorization': `Bearer ee4fecbcfa2218525f059ed37b42a0e44cadaf88`
+      'Authorization': `Bearer ${accessKey}`
     },
     body: (body === undefined || body === null) ? null : JSON.stringify(body)
   })
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok && response.status !== 422) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      }
+    })
 }
 
 export const getUser = () => {
-  return callAPI('/user','GET')
+  return callAPI('/user', 'GET')
 }
 
 export const getAllRepos = () => {
@@ -28,6 +35,6 @@ export const createRepo = (newRepo = { name: 'Repo-create-by-api' }) => {
   return callAPI('/user/repos', 'POST', newRepo)
 }
 
-export const deleteRepo = (owner, repoName) => {
-  return callAPI(`/repos/${owner}/${repoName}`, 'DELETE')
+export const deleteRepo = (repo) => {
+  return callAPI(`/repos/${repo.owner.login}/${repo.name}`, 'DELETE')
 }
